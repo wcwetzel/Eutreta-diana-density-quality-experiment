f73264b39064f4629c4e26a54119be3267e78b26
wcwetzel <wcwetzel@ucdavis.edu>
2011-09-23 14:02:03 -0700

    commenting

--- VAA 2010 results.R
+++ VAA 2010 results.R
@@ -1,22 +1,30 @@
-# VAA 2010 results
+###           VAA 2010 results               ###
+### density / host-plant quality experiment  ###
 
-d = read.csv('VAA map.csv')
+# 1st plotting and then model fitting for number of galls collected from each
+# experimental plant in the 2009-2010 density/quality experiment at Valentine.
+
+# load data, entered gall numbers on map dataframe
+d = read.csv('/Users/will/Documents/DATA/2011 DATA/field/VAA map.csv')
 
 library(lattice)
 library(rgl)
-library(bbmle)
 library(fields)
+library(scatterplot3d)
+library(bbmle)
+
 
 plot(galls2011 ~ I(females+ runif(30)), data=d)
 plot(galls2011 ~ I(natural.galls+ runif(30)), data=d)
 
-
-plot(galls2011[d$females>0 & d$shrubdying2011==0] ~ natural.galls[d$females>0 & d$shrubdying2011==0], data=d)
+plot(galls2011[d$females>0 & d$shrubdying2011==0] ~ natural.galls[d$females>0 & d$shrubdying2011==0],
+	data=d)
 plot(galls2011[d$shrubdying2011==0] ~ females[d$shrubdying2011==0], data=d)
 
-cloud( galls2011 ~ natural.galls + females, data=d)
+#cloud( galls2011 ~ natural.galls + females, data=d)
 
-plot3d(d$galls2011 ~ d$natural.galls + d$females, type='s', size=1, zlab='Number of galls', xlab='Previous year gall density',ylab='Number of females')
+plot3d(d$galls2011 ~ d$natural.galls + d$females, type='s', size=1, zlab='Number of galls', xlab=
+	'Previous year gall density',ylab='Number of females')
 plot3d(d$galls2011 ~ d$natural.galls + d$females, type='h', add=TRUE)
 
 plot3d(d$galls2011[d$females>0 & d$shrubdying2011==0] ~ d$natural.galls[d$females>0 & d$shrubdying2011==0]
@@ -41,19 +49,24 @@ mf5 = mle2(d$galls2011 ~ dnbinom(mu = exp(b * d$females), size=s),
 	start=list(b = 1, s=1), data=d)
 points(x=1:16, y = exp(coef(mf5)[1] * 1:16), type='l', col='green')
 
-mfg1 = mle2(d$galls2011 ~ dpois(lambda = a * (d$females + d$natural.galls)), data=d, start=list(a = 1/4))
+mfg1 = mle2(d$galls2011 ~ dpois(lambda = a * (d$females + d$natural.galls)), data=d, 
+	start=list(a = 1/4))
 abline(mfg1, lty=2)
 
-mfg2 = mle2(d$galls2011 ~ dnbinom(mu = a * (d$females + d$natural.galls), size = s), start=list(a = 1/4, s = 1), data=d)
+mfg2 = mle2(d$galls2011 ~ dnbinom(mu = a * (d$females + d$natural.galls), size = s), 
+	start=list(a = 1/4, s = 1), data=d)
 abline(mf2, col='blue', lty=2)
 
-mg1 = mle2(d$galls2011 ~ dpois(lambda = a * (d$natural.galls+1)), data=d, start=list(a = 1/4))
+mg1 = mle2(d$galls2011 ~ dpois(lambda = a * (d$natural.galls+1)), data=d, 
+	start=list(a = 1/4))
 abline(mfg1, lty=2)
 
-mg2 = mle2(d$galls2011 ~ dnbinom(mu = a * (1 + d$natural.galls), size = s), start=list(a = 1/4, s = 1), data=d)
+mg2 = mle2(d$galls2011 ~ dnbinom(mu = a * (1 + d$natural.galls), size = s), 
+	start=list(a = 1/4, s = 1), data=d)
 abline(mf2, col='blue', lty=2)
 
-mf2.1 = mle2(d$galls2011 ~ dnbinom(mu = a * d$females + b * (psi.r+.3), size = s), start=list(a = 1/4, b=1, s = 1), data=d)
+mf2.1 = mle2(d$galls2011 ~ dnbinom(mu = a * d$females + b * (psi.r+.3), size = s), 
+	start=list(a = 1/4, b=1, s = 1), data=d)
 abline(mf2, col='blue')
 
 AICtab(mf1, mf2, mf3, mf4, mf5, mfg1, mfg2, mg1, mg2)
