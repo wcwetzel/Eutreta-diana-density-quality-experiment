0f138db73eeb73616e29530be6e95168addae078
wcwetzel <wcwetzel@ucdavis.edu>
2011-09-24 20:20:26 -0700

    started adding QE models

--- modeling VAA 2010 results.R
+++ modeling VAA 2010 results.R
@@ -1,5 +1,83 @@
+# ~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/
+#	modeling VAA 2010 results.R
+
+###        modeling VAA 2010 results         ###
+### density / host-plant quality experiment  ###
+
+library(bbmle)
+library(emdbook)
+
+# load data, entered gall numbers on map data csv
+d = read.csv('~/Documents/DATA/2011 DATA/field/VAA map.csv')
+
+# using approach from qualifying exam
+# see 'qe presentation.pdf'
+# Ricker between number of females and number of galls (larvae)
+# quality variables added in exponent
+# N(t+1) = females * exp(r(1-females/k) + b1*quality1 + b2*quality2)
+# negbinom or zero-inflated nbinom stochasticity
+# going with nbinom instead of pois b/c var is several times the mean
+# also it makes sense that the lambda on each shrub would be gamma distributed
+
+# m0. intercept model
+m0 = mle2(galls2011 ~ dnbinom( mu = mu, size = s), start = list(mu = 
+	mean(d$galls2011), s=1), data=d)
+
+# m0.5. females, no density dependence
+m0.5 = mle2(galls2011 ~ dnbinom( mu = r * females, size=s ), 
+	start = list(r = 0.5, s=0.4), data = d)
+
+# m0.75 females, galls, no DD
+m0.75 = mle2(galls2011 ~ dnbinom( mu = r * females + b * natural.galls, size=s ), 
+	start = list(r = 0.5, s=0.4, b = 0), data = d)
+
+# m1. females, ricker
+m1 = mle2(galls2011 ~ dnbinom( mu = females * exp(r * (1 - females / k)), size=s ), 
+	start = list(r = -0.3, k = 4, s=0.4), data = d)
+
+# m2. females, beverton holt
+m2 = mle2(galls2011 ~ dnbinom( mu = r * females / (1 + a * females), size=s ), 
+	start = list(r = 0.8, a = 0.1, s=0.4), data = d)
+
+# m3. females, psi.r, ricker
+m3 = mle2(galls2011 ~ dnbinom( mu = females * exp(r * (1 - females / k) + 
+	b1 * psi.r), size=s ), start = list(r = -0.3, k = 4, s=0.4, b1=0), data = d)
+
+# m4. females, galls, ricker
+m4 = mle2(galls2011 ~ dnbinom( mu = females * exp(r * (1 - females / k) + 
+	b1 * natural.galls), size=s ), start = list(r = -0.3, k = 4, s=0.4, b1=0), 
+	data = d)
+
+# m5. females, galls, psi.r, ricker
+m5 = mle2(galls2011 ~ dnbinom( mu = females * exp(r * (1 - females / k) + 
+	b1 * natural.galls + b2 * psi.r), size=s ), start = list(r = -0.3, k = 4, s=0.4, 
+	b1=0, b2=0), data = d)
 
 
+
+
+AICtab(m0, m0.5, m1, m2, m3, m4, m5)
+
+plot(galls2011 ~ females, data=d)
+fp = 0:18
+gp = fp * exp(coef(m1)['r'] * (1 - fp / coef(m1)['k']))
+points(gp ~ fp, type='l')
+p1.smooth2 = p1.smooth + geom_smooth(aes(ymin = , ymax = ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################### OLD ###################################
 mf1 = mle2(d$galls2011 ~ dpois(lambda = a * d$females), data=d, start=list(a = 1/4))
 plot(galls2011 ~ females, data=d)
 abline(mf1)
@@ -50,5 +128,5 @@ mz1 = mle2(galls2011 ~ dzinbinom(mu = a * females, size = s, zprob = zprob),
 
 
 AICtab(mf1, mf2, mf3, mf4, mf5, mfg1, mfg2, mg1, mg2, mz1)
-
+##############################################################
 
--- plotting VAA 2010 results.R
+++ plotting VAA 2010 results.R
@@ -1,5 +1,5 @@
-# /Users/will/Documents/Analysis repos/
-# Eutreta-diana-density-quality-experiment/plotting VAA 2010 results.R
+# ~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/
+#	plotting VAA 2010 results.R
 
 ###        plotting VAA 2010 results         ###
 ### density / host-plant quality experiment  ###
@@ -8,16 +8,13 @@
 # plotting number of galls collected from each experimental plant 
 # in the 2009-2010 density/quality experiment at Valentine.
 
-# load data, entered gall numbers on map dataframe
+# load data, entered gall numbers on map data csv
 d = read.csv('~/Documents/DATA/2011 DATA/field/VAA map.csv')
 
 library(ggplot2)
-#library(lattice)
 library(rgl)
-library(fields)
 library(scatterplot3d)
 library(bbmle)
-library(Cairo)
 
 # plotting questions:
 #		1. should I include the 2 dying plants?
@@ -55,7 +52,8 @@ p2 = ggplot(data = d, aes(x = natural.galls, y = galls2011)) +
 p2.smooth = p2 + stat_smooth(method = 'loess', colour='royalblue', span=1)
 
 # save plot as a pdf
-ggsave('~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/galls~naturalgalls.pdf', 
+ggsave(
+	'~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/galls~naturalgalls.pdf', 
 	plot = p2.smooth, width=4, height=4)
 
 # 3. plot number of galls in spring 2011 ~ time corrected water potential
@@ -86,7 +84,8 @@ s3d1 = scatterplot3d(d$natural.galls, d$females, d$galls2011, pch=16, highlight.
 fit.pois = mle2(galls2011 ~ dpois(lambda = a + b * natural.galls + c * females), 
 	start = list(a = mean(d$galls2011), b = 0, c = 0), method = 'SANN', data=d)
 s3d1$plane3d(fit.pois)
-dev.print(pdf, file="~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/3d VAA 2010.pdf",
+dev.print(pdf, file=
+	"~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/3d VAA 2010.pdf",
 	width=5.1, height=5.1, pointsize=8) # width/height are in inches
 
 
