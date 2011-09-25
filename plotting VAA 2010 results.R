0ca54895acae2f8a251b88544fa2b5da0e6ca3a4
wcwetzel <wcwetzel@ucdavis.edu>
2011-09-24 18:03:39 -0700

    separated figs, saved as pdfs

--- plotting VAA 2010 results.R
+++ plotting VAA 2010 results.R
@@ -9,15 +9,15 @@
 # in the 2009-2010 density/quality experiment at Valentine.
 
 # load data, entered gall numbers on map dataframe
-d = read.csv('/Users/will/Documents/DATA/2011 DATA/field/VAA map.csv')
+d = read.csv('~/Documents/DATA/2011 DATA/field/VAA map.csv')
 
 library(ggplot2)
-library(lattice)
+#library(lattice)
 library(rgl)
 library(fields)
 library(scatterplot3d)
 library(bbmle)
-library(emdbook)
+library(Cairo)
 
 # plotting questions:
 #		1. should I include the 2 dying plants?
@@ -26,7 +26,7 @@ library(emdbook)
 
 ## first 2-dimensional plots
 # load someone's hack to allow suppression of top and right borders
-source("/Users/will/Documents/R/ggplot2/rnc_ggplot2_border_themes.r")
+source("~/Documents/R/ggplot2/rnc_ggplot2_border_themes.r")
 
 # 1. plot number of galls in spring 2011 ~ number of females introduced in summer 2010
 p1 = ggplot(data = d, aes(x = females, y = galls2011)) +
@@ -38,9 +38,10 @@ p1 = ggplot(data = d, aes(x = females, y = galls2011)) +
 		
 # now add a smoother with confidence interval, methods include loess, lm, glm (family='poisson')
 p1.smooth = p1 + stat_smooth(method = 'loess', colour='royalblue', span=1) 
-png('/Users/will/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/galls~females.png')
-print(p1.smooth)
-dev.off()
+
+# save plot as pdf
+ggsave('~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/galls~females.pdf', 
+	plot = p1.smooth, width=4, height=4)
 
 # 2. plot number of galls in spring 2011 ~ 
 #		number of naturally occuring, experimentally removed 2010 galls
@@ -54,7 +55,7 @@ p2 = ggplot(data = d, aes(x = natural.galls, y = galls2011)) +
 p2.smooth = p2 + stat_smooth(method = 'loess', colour='royalblue', span=1)
 
 # save plot as a pdf
-ggsave('/Users/will/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/galls~naturalgalls.png', 
+ggsave('~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/galls~naturalgalls.pdf', 
 	plot = p2.smooth, width=4, height=4)
 
 # 3. plot number of galls in spring 2011 ~ time corrected water potential
@@ -68,16 +69,16 @@ p3 = ggplot(data = d, aes(x = psi.r, y = galls2011)) +
 		
 # now add a smoother with confidence interval, methods include loess, lm, glm (family='poisson')
 p3.smooth = p3 + stat_smooth(method = 'loess', colour='royalblue', span=1)
-png('/Users/will/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/galls~rpsi.png')
-print(p3.smooth)
-dev.off()
+
+# save plot as pdf
+ggsave('~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/galls~rpsi.pdf', 
+	plot = p3.smooth, width=4, height=4)
 
 ## second 3-d plots
 
 # 1. plot galls on plants in spring 2011 ~ removed natural galls and introduced females
 # highlight.3d colors points in relation to y axis, the number of females
 # type='h' displays the height of each point
-png('/Users/will/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/3d-VAA2010.png')
 s3d1 = scatterplot3d(d$natural.galls, d$females, d$galls2011, pch=16, highlight.3d=TRUE, 
 	type='h', zlab = 'Galls in year t+1', ylab = 'Introduced females', 
 	xlab = 'Galls in year t')
@@ -85,8 +86,9 @@ s3d1 = scatterplot3d(d$natural.galls, d$females, d$galls2011, pch=16, highlight.
 fit.pois = mle2(galls2011 ~ dpois(lambda = a + b * natural.galls + c * females), 
 	start = list(a = mean(d$galls2011), b = 0, c = 0), method = 'SANN', data=d)
 s3d1$plane3d(fit.pois)
-dev.print(png, file="~/RGraph.png", width=5.1, height=5.1, pointsize=8) # width/height are in inches
-dev.off()
+dev.print(pdf, file="~/Documents/Analysis repos/Eutreta-diana-density-quality-experiment/figs/3d VAA 2010.pdf",
+	width=5.1, height=5.1, pointsize=8) # width/height are in inches
+
 
 # 2. interactive version of above plot
 plot3d(d$galls2011 ~ d$natural.galls + d$females, type='s', size=1, zlab='Number of galls', xlab=
